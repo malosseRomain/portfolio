b97b4209c2bc21e4103c9d6c07df6215b0e87365
malosseRomain <romain.malosse43@icloud.com>
2024-10-25 20:25:30 +0200

    Remove the need of double click on section "Projets"

--- assets/js/switchProjectPage.js
+++ assets/js/switchProjectPage.js
@@ -1,6 +1,6 @@
 function switchProjectPage() {
     const currentProjectNumber = parseInt(window.location.pathname.match(/Project(\d+)\.html$/)[1]);
-    const totalProjects = 10;
+    const totalProjects = 8;
     const nextProjectNumber = (currentProjectNumber % totalProjects) + 1;
     const prevProjectNumber = (currentProjectNumber === 1) ? totalProjects : currentProjectNumber - 1;
 
